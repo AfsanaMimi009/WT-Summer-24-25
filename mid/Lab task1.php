9824535cab7d20bee3e908d4f9301f7c1b22660d
AfsanaMimi009 <116268093+AfsanaMimi009@users.noreply.github.com>
2025-07-20 02:49:12 -0700

    Add files via upload

---
+++ mid/Lab task1.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<title>This the a registration form </title>
+<body>
+<center>
+<h1 style="color:blue;">AIUB management System </h1>
+<h3 style="color:blue;"> Registration Form </h3>
+</center>
+<h3 align="left"> Complete the Registration </h3>
+<table>
+    <tr>
+        <td> Enter Your Name:</td>
+       <td><input type ="text"></td>
+</tr>
+<tr>
+    <td>Email:</td>
+    <td><input type="text"  /></td>
+       </tr>
+
+
+
+<tr>
+    <td>Password:</td>
+    <td><input type="password"  /></td>
+    </tr>
+
+<tr>
+    <td>Gender:</td>
+    <td><input type="radio" name="des"  value=""> Male </td>
+
+    <td><input type="radio" name="des"  value=""> Other </td>
+               
+    </tr>
+<tr>
+    <td>  Language known :</td>
+    <td><input type="radio" name="des">Bangla </td>
+    <td><input type="radio" name="des">English </td>
+    <td><input type="radio" name="des">Hindi </td>
+    
+</tr>
+<tr>
+    <td>Country</td>
+    <td>
+    <Select name="Select">
+    <option value="text">Select</option>
+    </Select>
+</td>
+</tr>
+<tr>
+  <td>Date of Birth:</td>
+  <td><input type="Date"  /></td>
+  </tr>
+  <tr>
+     <td> Upload Photo</td>
+    <td><input type="file" name="" value="" /></td>
+     </tr>
+
+  <tr>
+    <td>Comment </td>
+     <td><input type="Text"/> </td>
+   
+
+     <tr>
+         <td></td>
+            <td>
+             <input type="submit" name="" value="register"  />
+                              
+             </td>
+         </tr>
+
+ 
+</body>
+</html>
